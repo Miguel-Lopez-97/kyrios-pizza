423b3fc8c0363c909f70cd1d0e20a2c87b8a8fd9
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-02 12:25:20 -0500

    Fix Header fixed position

--- src/Components/Main/Header/Header.jsx
+++ src/Components/Main/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './Header.css';
+import "./Header.css";
 import { NavBar } from "./NavBar/NavBar";
 
-export function Header(){
-
-    return(
-        <NavBar/>
-    );
+export function Header() {
+  return (
+    <header>
+      <NavBar />
+    </header>
+  );
 }
