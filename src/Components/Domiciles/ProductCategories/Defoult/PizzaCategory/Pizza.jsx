382dc220302d4f71a13e547f17c008cf143821a2
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-11 10:49:33 -0500

    Data img change url size pizza

--- src/Components/Domiciles/ProductCategories/Defoult/PizzaCategory/Pizza.jsx
+++ src/Components/Domiciles/ProductCategories/Defoult/PizzaCategory/Pizza.jsx
@@ -10,7 +10,7 @@ export function Pizza() {
       <div className="pizzasSizes">
         <div className="pizzaSize">
           <div className="pizza xl">
-              <img src="https://drive.google.com/uc?export=download&id=1j6LU9_-Ht9YbjQsRtI_vKHgWMB96YzeR" alt="Pizza" />
+              <img src="https://drive.google.com/uc?export=download&id=1RL33TKltp4cmYxjovgFc7XEpULWWB-FD" alt="Pizza" />
           </div>
           <div className="pizzaSizeInfo">
               <h2>XL<br/>$40.000</h2>
@@ -18,7 +18,7 @@ export function Pizza() {
         </div>
         <div className="pizzaSize">
           <div className="pizza small">
-          <img src="https://drive.google.com/uc?export=download&id=1E7iV5GcDx6pyIJGkB-BC9cKtuCpvzZF3" alt="Pizza" />
+          <img src="https://drive.google.com/uc?export=download&id=1482kWIOqIidYOGiZlnaeZpk3md7ie9Y0" alt="Pizza" />
           </div>
           <div className="pizzaSizeInfo">
           <h2>Small<br/>$31.000</h2>
@@ -26,7 +26,7 @@ export function Pizza() {
         </div>
         <div className="pizzaSize">
           <div className="pizza ms">
-          <img src="https://drive.google.com/uc?export=download&id=1Ql9p6DDqvkPJ6odqIU3ZDE8omlpcgeC_" alt="Pizza" />
+          <img src="https://drive.google.com/uc?export=download&id=1lkayyscLeU_kdLN_e-xzkobcmi2jjUhk" alt="Pizza" />
           </div>
           <div className="pizzaSizeInfo">
           <h2>MS<br/>$22.000</h2>
@@ -34,7 +34,7 @@ export function Pizza() {
         </div>
         <div className="pizzaSize">
           <div className="pizza individual">
-          <img src="https://drive.google.com/uc?export=download&id=1WVD6-RGKcSUwLOlQ2hSB9r0tbOGyu7QI" alt="Pizza" />
+          <img src="https://drive.google.com/uc?export=download&id=1y4WT87JgcdSSF9JCmNzzyHGHRelXb5bU" alt="Pizza" />
           </div>
           <div className="pizzaSizeInfo">
           <h2>Personal<br/>$5.000</h2>
